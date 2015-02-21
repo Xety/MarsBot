aaa7019ec060da0a863bdc2626c346d55c21573b
Xeta <emeric.f@noze.tv>
2015-02-21 08:40:05 +0100

    Added Socket Exception

---
+++ src/Core/Network/Exception/SocketException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Noze\Core\Network\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception class for Socket. This exception will be thrown from Socket when it encounters an error.
+ *
+ */
+class SocketException extends RuntimeException {
+}
